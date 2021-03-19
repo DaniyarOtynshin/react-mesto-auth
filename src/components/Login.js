f778525b43511b115829066cd215726ffbff26fd
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-03-20 01:56:35 +0600

    feat: add Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,42 @@
+import PopupWithForm from './PopupWithForm';
+
+const [login, setLogin] = React.useState('');
+const [password, setPassword] = React.useState('');
+
+function handleLoginSubmit(e) {
+    e.preventDefault();
+    props.onLogin({
+      login, password
+    });
+    handleClose();
+}
+
+function handleClose() {
+    props.onClose()
+    setLogin('');
+    setPassword('');
+}
+
+function handleLogin(e) {
+    setLogin(e.target.value);
+}
+
+function handlePassword(e) {
+    setPassword(e.target.value);
+}
+
 const Login = () => {
     return (
-        Login
+        <PopupWithForm name="login" isOpen={props.isOpen} title="Логин" onClose={handleClose} buttonText="Login" onSubmit={handleLoginSubmit}>
+            <section className="popup__section">
+                <input type="text" minLength="2" maxLength="30" name="login" id="title-input" required placeholder="Логин" className="popup__input" value={login} onChange={handleLogin} />
+                <span className="popup__input-error" id="title-input-error"></span>
+            </section>
+            <section className="popup__section">
+                <input type="password" name="password" id="link-input" required placeholder="Пароль" className="popup__input" value={password} onChange={handlePassword} />
+                <span className="popup__input-error" id="link-input-error"></span>
+            </section>
+        </PopupWithForm>
     )
 }
 
