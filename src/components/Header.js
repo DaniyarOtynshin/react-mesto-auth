a92107a4d07165ae970bc02b5b10e8bc5d826c71
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-03-22 02:09:00 +0600

    feat: add status to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ function Header() {
     return (
         <header className="header page__header">
             <img src={logoPath} alt="логотип" className="header__logo" />
+            <p className="header__auth">Регистрация</p>
         </header>
     )
 }
